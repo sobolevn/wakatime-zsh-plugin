798051b1050f67faf12d352b3f0f85e70758a3aa
Enrico Deleo <hello@enricodeleo.com>
2021-10-27 00:18:16 +0200

    feat: support wakatime-cli's custom path

--- wakatime.plugin.zsh
+++ wakatime.plugin.zsh
@@ -11,12 +11,18 @@ _wakatime_heartbeat() {
     return  # Tracking is skipped!
   fi
 
+  # Set a custom path for the wakatime-cli binary
+  # otherwise point to the default `wakatime`
+  local wakatime_bin="${ZSH_WAKATIME_BIN:='wakatime'}"
+
   # Checks if `wakatime` is installed,
-  # syntax is `zsh` specific, see: https://unix.stackexchange.com/a/237084
-  if (( ! $+commands[wakatime] )); then
+  if ! wakatime_loc="$(type -p "$wakatime_bin")" || [[ -z $wakatime_bin ]]; then
     echo 'wakatime cli is not installed, run:'
     echo '$ pip install wakatime'
-    echo 'Or check that wakatime is in PATH'
+    echo
+    echo 'OR:'
+    echo 'Option 1: Check that wakatime is in PATH'
+    echo 'Option 2: Set a custom path with $ export ZSH_WAKATIME_BIN=/path/to/wakatime-cli'
     echo
     echo 'Time is not tracked for now.'
     return
@@ -57,7 +63,7 @@ _wakatime_heartbeat() {
     should_work_online=''
   fi
 
-  wakatime --write \
+  $wakatime_bin --write \
     --plugin 'wakatime-zsh-plugin/0.2.1' \
     --entity-type app \
     --entity "$last_command" \
