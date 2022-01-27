2ced689a3d2d72cc3311968340c4a743367fd086
Adewale Dharmy™ <dammysteve10@gmail.com>
2022-01-27 11:54:56 +0100

    Fixed Issue With The Plugin Not Finding The Wakatime Executable (#17)

--- wakatime.plugin.zsh
+++ wakatime.plugin.zsh
@@ -13,7 +13,7 @@ _wakatime_heartbeat() {
 
   # Set a custom path for the wakatime-cli binary
   # otherwise point to the default `wakatime`
-  local wakatime_bin="${ZSH_WAKATIME_BIN:='wakatime'}"
+  local wakatime_bin="${ZSH_WAKATIME_BIN:=wakatime}"
 
   # Checks if `wakatime` is installed,
   if ! wakatime_loc="$(type -p "$wakatime_bin")"; then
